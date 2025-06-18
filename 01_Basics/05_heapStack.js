adb078366b2998c82a0ddfdb698705a3d7694e89
BhalkeVaibhav <130219136+BhalkeVaibhav@users.noreply.github.com>
2025-06-18 10:33:53 +0000

    heap stack

---
+++ 01_Basics/05_heapStack.js
@@ -0,0 +1,57 @@
+// stack and heap
+// there are two thype of data type one is premitive and 2nd in non premitive 
+// in premetev daya type memory save in stack memory
+// in non premitive datatype memory save in heap memory
+
+// prmitive datatype
+// 1 String
+// 2 Number
+// 3 bigint
+// 4 boolean
+// 5 null
+// 6 undefined
+// 7 Symbole
+
+// non primitive
+// 1 Array
+// 2 Object
+// 3 function
+
+// javascript is a dynamicly type language because in javascript we dose not define datatype javascript browser will give them at the time of requestAnimationFrame
+// when memory store in stack memory and the stack give you copy file of variable 
+//  when memory store in heap memory then heap memory give you refrence of orignal value
+
+let strinG="Vaibhav";
+let num=1231;
+let bigINT=323535355n;
+let bool=true;
+let nu="null";
+let undef;
+
+// non premitive
+// array
+let arr=["Shaktiman","Superman","Iranman"]
+console.log(arr);
+
+// object
+let objectName={
+    name:"vaibhav",
+    id:2,
+    stydy:"MCA",
+
+}
+console.log(objectName);
+
+// function
+let myFunction=function(){
+    console.log(arr);
+}
+
+console.log(typeof (arr));
+console.log(typeof(objectName) );
+console.log(typeof(myFunction) );
+
+let email=""
+
+
+
