cb790edafa0b0aa6bb7ab2ac2fd7288e299a0e5a
BhalkeVaibhav <130219136+BhalkeVaibhav@users.noreply.github.com>
2025-06-18 06:36:53 +0000

    variable and datatype

---
+++ 01_Basics/01Variable.js
@@ -0,0 +1,14 @@
+const password = 123456789;
+let email = "vaibhav123@gmail.com";
+var city = "Nagar";
+
+// do not use var variable anyware becase this is old and less secure use only let and const
+/*
+  let is access in only block
+  const dose not reassigne or change the value of const variable
+ */
+
+console.log(password);
+
+//When you want print more than 1 output in singal line then use console.table([, , ,])
+console.table([password,email,city]);
---
+++ 01_Basics/02_Datatype.js
@@ -0,0 +1,24 @@
+"use strict"// Whwn we use that the browser will treat as new version of javascript
+
+//alert(3 + 3); //the alert is not use hear because of we are using node you want use it then use on browser console windo or other
+
+let name="Vaibhav";
+let number = 18;
+let isLOgedIn = true;
+let state;
+
+   // premitive   
+// number => 2 to the power of 53
+// bigint
+// string
+// boolean => true or false
+// null => Standalone value
+// undefine
+// symbole => Uniqueness
+
+        // non pemitive
+  //oblect
+  //
+
+
+  console.log(typeof name);
