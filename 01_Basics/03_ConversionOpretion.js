a38c1d36306e2440ba4d7489a5dfda9bab6924f1
BhalkeVaibhav <130219136+BhalkeVaibhav@users.noreply.github.com>
2025-06-18 07:03:33 +0000

    Conversion

---
+++ 01_Basics/03_ConversionOpretion.js
@@ -0,0 +1,19 @@
+let name="Vaibhav";
+let num=123;
+let logedIn=true;
+
+console.log(typeof name);
+console.log(typeof(name));
+console.log(typeof(num));
+console.log(typeof(logedIn));
+
+let name2 = Number(name);
+console.log(typeof name2); //Number
+console.log(name2);// NaN
+
+let num2 = Number(num); //number
+console.log(num2); //
+console.log(typeof(num2));//Number
+console.log( Boolean(num));//true
+
+//33
